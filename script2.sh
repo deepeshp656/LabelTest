2fbe34fc9c6c0a5ce958582ec07c83a375a1ba40
Deepesh Patel <Deepeshp656@gmail.com>
2021-01-12 17:21:14 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,7 +1,7 @@
 #Define variable to store result of git diff and pattmatch state of matching files
             PattFound="False"      
             
-            CHANGED_FILES=$(git diff --name-only remotes/origin/${{ github.base_ref }}...remotes/origin/${{ github.head_ref }} )
+            CHANGED_FILES=$(git diff --name-only remotes/origin/$Base...remotes/origin/$Head )
             
             #changing format of variable to array 
             CHANGED_FILES_ARR=($CHANGED_FILES)
