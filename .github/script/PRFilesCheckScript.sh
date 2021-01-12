daf85ae49b1dab1b50e776cea2d44033adb00986
Deepesh Patel <Deepeshp656@gmail.com>
2021-01-13 00:21:06 +0530

    Add files via upload

--- .github/script/PRFilesCheckScript.sh
+++ .github/script/PRFilesCheckScript.sh
@@ -5,14 +5,14 @@ PattFound="False"
 CHANGED_FILES="$(git diff --name-only origin/"$BASEBRANCH"...origin/"$HEADBRANCH" )"
 
 #changing format of variable to array 
-CHANGED_FILES_ARR="($CHANGED_FILES)"
+CHANGED_FILES_ARR=("$CHANGED_FILES")
 Path=("$TGT_PATH")
 echo $Path
 echo $CHANGED_FILES_ARR
 echo $CHANGED_FILES
 
 #iterate each file to match with pattern mentioned in TGT_Files in format of path/prefix-of-file or path
-for i in "${CHANGED_FILES[@]}"
+for i in "${CHANGED_FILES_ARR[@]}"
   do
   
 	 for j in "${Path[@]}"
