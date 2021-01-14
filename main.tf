f8c650a8799d12638a0c8db3afc0804229dced7b
Deepesh Patel <Deepeshp656@gmail.com>
2021-01-14 11:20:43 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
+
+
+  backend "remote" {
+    organization = "Deepesh-Test"
+
+    workspaces {
+      name = "Terraform-test2"
+    }
+  }
+ }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+ 
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
